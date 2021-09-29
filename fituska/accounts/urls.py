eb07a50a86f6e538e9855c56046d5e0e36c34de4
Filip Solich <filip.solich@tuta.io>
2021-09-29 20:01:34 +0200

    Change urls in accounts

--- fituska/accounts/urls.py
+++ fituska/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from .views import LoginView, SignUpView
+from .views import LoginView, SignUpView, make_moderator
 
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', auth_views.logout_then_login, name='logout'),
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('make_moderator/<int:user_id>/', make_moderator, name='make_moderator'),
 ]

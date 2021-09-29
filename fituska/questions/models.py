7f735e97112414fb72f4c0e6a5777480269513f6
Sechma <marokac@seznam.cz>
2021-09-29 21:15:15 +0200

    added in /questions/models.py classes(Answer,Reaction) for database and in requirements Pillow for ImageField

--- fituska/questions/models.py
+++ fituska/questions/models.py
@@ -3,11 +3,30 @@ from django.db import models
 from accounts.models import User
 
 class Answer(models.Model):
-    pass
+
+    text = models.TextField(blank=False,max_length=10000)
+    picture = models.ImageField(blank=True)
+    validity = models.BooleanField('type',blank=True)
+    points = models.DecimalField(max_digits=5,decimal_places=0, blank=True)
+    teacher_points = models.DecimalField(max_digits=5,decimal_places=0,blank=True)
+
+    user = models.ForeignKey(User, on_delete=models.PROTECT) # can be cascade
+    #question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+
+class Reaction(models.Model):
+
+    text = models.TextField(blank=False,max_length=10000)
+    picture = models.ImageField(blank=True)
+
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+
 
 class Rating(models.Model):
 
     type = models.BooleanField('type')
+
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
 

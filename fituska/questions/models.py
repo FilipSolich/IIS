618fc77a4ff5af3260e7d0b9e1dda8f1bb770818
Filip Solich <filip.solich@tuta.io>
2021-09-29 14:02:39 +0200

    Add rating model

--- fituska/questions/models.py
+++ fituska/questions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from accounts.model import User
+
+
+class Rating(models.Model):
+
+    type = models.BooleanField('type')
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+
+
+class Answer(models.Model):
+    pass

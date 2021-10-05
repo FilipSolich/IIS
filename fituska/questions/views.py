a4d4bf8970ac4a1161165fc3aaad9793e65219a9
Filip Solich <filip.solich@tuta.io>
2021-10-05 15:47:00 +0200

    Add urls for subject

--- fituska/questions/views.py
+++ fituska/questions/views.py
@@ -1,21 +1,35 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 
 from .models import Answer, Question, Rating
+from accounts.decorators import teacher_required
 from subjects.models import Subject
 
 
 # TODO Dodelat vyhledavani
-def questions_list(request, subject_name):
-    subject = get_object_or_404(Subject, name=subject_name)
+def list_questions(request, shortcut, year):
+    subject = get_object_or_404(Subject, shortcut=shortcut, year=year)
     questions = Question.objects.filter(subject=subject)
     return render(request, 'questions/questions.html', {'questions': questions})
 
 
-def question_detail(request, subject_name, question_id):
+def detail_question(request, shortcut, year, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'questions/question.html', {'questions': question})
+
+
+@teacher_required
+@require_POST
+def confirm_answer(request, shortcut, year, question_id, answer_id):
+    pass
+
+
+@teacher_required
+@require_POST
+def reject_answer(request, shortcut, year, question_id, answer_id):
+    pass
 
 
 def _add_points_to_answer(answer, type_, value=1):
@@ -27,8 +41,9 @@ def _add_points_to_answer(answer, type_, value=1):
     answer.save()
 
 
+@require_POST
 @login_required
-def answer_rate(request, answer_id, **kwargs):
+def rate_answer(request, shortcut, year, question_id, answer_id):
     answer = get_object_or_404(Answer, pk=answer_id)
     type_ = request.POST.get('type')
     rate = Rating.objects.get(user=request.user, answer=answer)

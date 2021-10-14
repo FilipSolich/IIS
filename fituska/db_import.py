ec6b3dd80e8d406ac018ff05d03ad413064f7415
Filip Solich <filip.solich@tuta.io>
2021-10-14 11:16:21 +0200

    Add registration DB import

--- fituska/db_import.py
+++ fituska/db_import.py
@@ -9,65 +9,53 @@ def save(lst):
         item.save()
 
 
+mod = Group.objects.get(name='Moderators')
+
 users = []
 
-u1 = User.objects.create(login='xnovak00',email='novacek@seznam.cz',first_name='Filip',last_name="Novák")
-u1.set_password("filip_novak")
+u1 = User.objects.create(login='xnovak00', email='novacek@seznam.cz', first_name='Filip', last_name='Novák', is_superuser=True)
+u1.set_password('filip_novak')
 users.append(u1)
 
-u2 = User.objects.create(login='xsechr00',email='marokac@seznam.cz',first_name='Marek',last_name="Sechra")
-u2.set_password("marek_sechra")
+u2 = User.objects.create(login='xsechr00',email='marokac@seznam.cz', first_name='Marek', last_name='Sechra')
+u2.set_password('marek_sechra')
+u2.groups.add(mod)
 users.append(u2)
 
-u3 = User.objects.create(login='xsolich00',email='solich@tutanota.cz',first_name='Filip',last_name="Solich")
-u3.set_password("filip_solich")
+u3 = User.objects.create(login='xsolich00', email='solich@tutanota.cz', first_name='Filip', last_name='Solich')
+u3.set_password('filip_solich')
+u3.groups.add(mod)
 users.append(u3)
 
-u4 = User.objects.create(login='xvokal00',email='d.vokal@gmail.com',first_name='Daniel',last_name="Vokál")
+u4 = User.objects.create(login='xvokal00', email='d.vokal@gmail.com', first_name='Daniel', last_name='Vokál')
+u3.set_password('nejakeheslo1')
 users.append(u4)
 
-u5 = User.objects.create(login='xnamys00',email='v.myslitel@seznam.cz',first_name='Vojtěch',last_name="Namyslo")
+u5 = User.objects.create(login='xnamys00', email='v.myslitel@seznam.cz', first_name='Vojtěch', last_name='Namyslo')
+u3.set_password('nejakeheslo2')
 users.append(u5)
-
-u6 = User.objects.create(login='xbohat00',email='d.bohaty@gmail.com',first_name='Damián',last_name="Bohatý")
-users.append(u6)
-
-u7 = User.objects.create(login='xsvaty00',email='svaty42@seznam.cz',first_name='Lukáš',last_name="Svatý")
-users.append(u7)
-
-u8 = User.objects.create(login='xsvetl00',email='karol.svet@gmail.com',first_name='Karolína',last_name="Světlá")
-users.append(u8)
-
-u10 = User.objects.create(login='xnemc00',email='babicka@centrum.cz',first_name='Božena',last_name="Němcová")
-users.append(u10)
-
-u9 = User.objects.create(login='xbalk00',email='m.balek00gmail.com',first_name='Mirek',last_name="Bálek")
-users.append(u9)
-
-u11 = User.objects.create(login='xsechr01',email='d.sechra@seznam.cz',first_name='David',last_name="Sechra")
-users.append(u11)
 
 save(users)
 
 
 subjects = []
 
-s1 = Subject.objects.create(name='Diskretní matematika', year ='2020', semester = 'winter', shortcut ='IDM', grade = 1, compulsory ='compulsory')
+s1 = Subject.objects.create(name='Diskretní matematika', year='2020', semester='winter', shortcut='IDM', grade=1, compulsory='compulsory')
 subjects.append(s1)
 
-s2 = Subject.objects.create(name='Diskretní matematika', year ='2019', semester = 'winter', shortcut ='IDM', grade = 1,compulsory ='compulsory' )
+s2 = Subject.objects.create(name='Diskretní matematika', year='2019', semester='winter', shortcut='IDM', grade=1,compulsory='compulsory' )
 subjects.append(s2)
 
-s3 = Subject.objects.create(name='Diskretní matematika', year ='2018', semester = 'winter', shortcut ='IDM', grade = 1, compulsory ='compulsory' )
+s3 = Subject.objects.create(name='Diskretní matematika', year='2018', semester='winter', shortcut='IDM', grade=1, compulsory='compulsory' )
 subjects.append(s3)
 
-s4 = Subject.objects.create(name='Linearní algebra', year ='2020', semester = 'winter', shortcut ='ILG', grade = 1, compulsory ='compulsory' )
+s4 = Subject.objects.create(name='Linearní algebra', year='2020', semester='winter', shortcut='ILG', grade=1, compulsory='compulsory' )
 subjects.append(s4)
 
-s5 = Subject.objects.create(name='Softwarové inženýrství', year ='2020', semester = 'winter', shortcut ='IUS', grade = 1, compulsory ='compulsory' )
+s5 = Subject.objects.create(name='Softwarové inženýrství', year='2020', semester='winter', shortcut='IUS', grade=1, compulsory='compulsory' )
 subjects.append(s5)
 
-s6 = Subject.objects.create(name='Základy programování', year ='2020', semester = 'winter', shortcut ='IZP', grade = 1, compulsory ='compulsory' )
+s6 = Subject.objects.create(name='Základy programování', year='2020', semester='winter', shortcut='IZP', grade=1, compulsory='compulsory' )
 subjects.append(s6)
 
 s7 = Subject.objects.create(name='Elektronika pro informační technologie', year ='2020', semester = 'winter', shortcut ='IEL', grade = 1, compulsory ='compulsory' )
@@ -120,37 +108,63 @@ karma.append(k1)
 k2 = Karma.objects.create(karma=15, user=u1, subject=s2)
 karma.append(k2)
 
-k3 = Karma.objects.create(karma=100, user=u1, subject=s3)
+k3 = Karma.objects.create(karma=4, user=u2, subject=s3)
 karma.append(k3)
 
-k4 = Karma.objects.create(karma=20, user=u3, subject=s10)
+k4 = Karma.objects.create(karma=15, user=u2, subject=s4)
 karma.append(k4)
 
-k5 = Karma.objects.create(karma=13, user=u4, subject=s11)
+k5 = Karma.objects.create(karma=20, user=u3, subject=s1)
 karma.append(k5)
 
-k6 = Karma.objects.create(karma=2, user=u3, subject=s9)
+k6 = Karma.objects.create(karma=2, user=u3, subject=s2)
 karma.append(k6)
 
-k7 = Karma.objects.create(karma=4, user=u2, subject=s8)
+k7 = Karma.objects.create(karma=13, user=u4, subject=s3)
 karma.append(k7)
 
-k8 = Karma.objects.create(karma=15, user=u2, subject=s7)
+k8 = Karma.objects.create(karma=99, user=u4, subject=s4)
 karma.append(k8)
 
-k9 = Karma.objects.create(karma=200, user=u5, subject=s6)
-karma.append(k9)
-
-k10 = Karma.objects.create(karma=5, user=u6, subject=s5)
-karma.append(k10)
-
-k11 = Karma.objects.create(karma=1, user=u8, subject=s2)
-karma.append(k11)
-
-k12 = Karma.objects.create(karma=99, user=u5, subject=s4)
-karma.append(k12)
-
-k13 = Karma.objects.create(karma=40, user=u3, subject=s12)
-karma.append(k13)
-
 save(karma)
+
+
+registrations = []
+
+r1 = Registration.objects.create(confirmed=True, user=u1, Subject=s1)
+registrations.append(r1)
+
+r2 = Registration.objects.create(confirmed=True, user=u1, Subject=s2)
+registrations.append(r2)
+
+r3 = Registration.objects.create(confirmed=True, user=u2, Subject=s3)
+registrations.append(r3)
+
+r4 = Registration.objects.create(confirmed=True, user=u2, Subject=s4)
+registrations.append(r4)
+
+r5 = Registration.objects.create(confirmed=True, user=u3, Subject=s2)
+registrations.append(r5)
+
+r6 = Registration.objects.create(confirmed=True, user=u3, Subject=s3)
+registrations.append(r6)
+
+r7 = Registration.objects.create(confirmed=True, user=u4, Subject=s4)
+registrations.append(r7)
+
+r8 = Registration.objects.create(confirmed=True, user=u4, Subject=s5)
+registrations.append(r8)
+
+r9 = Registration.objects.create(confirmed=True, user=u5, Subject=s6)
+registrations.append(r9)
+
+r10 = Registration.objects.create(confirmed=False, user=u5, Subject=s2)
+registrations.append(r10)
+
+r11 = Registration.objects.create(confirmed=False, user=u4, Subject=s5)
+registrations.append(r11)
+
+r12 = Registration.objects.create(confirmed=False, user=u5, Subject=s5)
+registrations.append(r12)
+
+save(registrations)

57ba0bb48edb2a821ea2d1f31e774d850a6f5f8c
Sechma <marokac@seznam.cz>
2021-10-18 23:28:02 +0200

    repaired fnc check_pass()

--- fituska/static/js/fnc.js
+++ fituska/static/js/fnc.js
@@ -2,8 +2,17 @@
 
 function check_pass()
 {
+	console.log(id_password1);
+	console.log(id_password2);
+
 	if ( document.getElementById('id_password2').value != document.getElementById('id_password1').value ){
 		document.getElementById('msg').style.color = 'red';
 		document.getElementById('msg').innerHTML = 'Zadeli jste jiná hesla!';
+		document.getElementById('msg').style.display = 'block';
+		document.getElementById('submit').style.display = 'none';
+	}
+	else{
+		document.getElementById('msg').style.display = 'none';
+		document.getElementById('submit').style.display = 'block';
 	}
 }

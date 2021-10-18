646dc09a44958f61ec8a9663019fc3f48b79eabb
Sechma <marokac@seznam.cz>
2021-10-18 23:28:49 +0200

    deleted logs

--- fituska/static/js/fnc.js
+++ fituska/static/js/fnc.js
@@ -2,9 +2,6 @@
 
 function check_pass()
 {
-	console.log(id_password1);
-	console.log(id_password2);
-
 	if ( document.getElementById('id_password2').value != document.getElementById('id_password1').value ){
 		document.getElementById('msg').style.color = 'red';
 		document.getElementById('msg').innerHTML = 'Zadeli jste jiná hesla!';

1d4c7d377c05581c61ec736a72f64204bb558496
Filip Solich <filip.solich@tuta.io>
2021-11-09 21:34:42 +0100

    Heroku settings

--- fituska/fituska/settings.py
+++ fituska/fituska/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -139,3 +141,7 @@ LOGIN_REDIRECT_URL = '/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Heroku settings
+
+django_heroku.settings(locals())
